5cc2d57275ae75b1ee3ebedddc8aea5ab549784f
SContent44 <23169220+SContent44@users.noreply.github.com>
2020-06-15 08:10:43 +1000

    feat: remove flickr since only works local

--- 0.2daa725c.js
+++ 0.2daa725c.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
 
 /***/ "./src/Components/grandparent/grandparent.css":
 /*!****************************************************!*\
--- 1.741667e9.js
+++ 1.741667e9.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
 
 /***/ "./src/Components/parent/index.js":
 /*!****************************************!*\
